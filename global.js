51f20ad87b41cb2b051906a8fb66e9958cc72709
Michelle Schulp <michelle@marktimemedia.com>
2015-05-11 14:58:05 -0500

    pulling some vars out of the scroll event, thanks joshbroton

--- global.js
+++ global.js
@@ -30,11 +30,11 @@
 /* Scroll Header */
 
 	var lastScrollTop = $(window).scrollTop(); // reset variable any time it reloads
+	var siteHeader = $('.site_header');
+	var siteTitle = $('.site_title');
+	var contentTop = $('#site-wrapper');
 
 	$(window).on('scroll', (function(event) {
-		var siteHeader = $('.site_header');
-		var siteTitle = $('.site_title');
-		var contentTop = $('#site-wrapper');
 		var scrollPosition = $(this).scrollTop();
 
 		if($(window).width() > 488 ) { // we're mobile first so this is anything larger than our mobile breakpoint
